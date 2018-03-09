232efa769a8e1c5f1e13e538550989277238a148
KaustubhPatange <developerkp16@gmail.com>
2018-03-09 11:55:58 +0530

    Added Device Checker Tool

---
+++ DeviceChecker/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Dell
+ * Date: 3/9/2018
+ * Time: 11:31 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace DeviceChecker
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label status;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.status = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(378, 68);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.Location = new System.Drawing.Point(44, 21);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(291, 27);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Check Whether Device is Connected or Not";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Red;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 86);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(39, 27);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(57, 92);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(23, 17);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "---";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 126);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(52, 17);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Status:";
+			// 
+			// status
+			// 
+			this.status.AutoSize = true;
+			this.status.Location = new System.Drawing.Point(70, 126);
+			this.status.Name = "status";
+			this.status.Size = new System.Drawing.Size(23, 17);
+			this.status.TabIndex = 4;
+			this.status.Text = "---";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(402, 156);
+			this.Controls.Add(this.status);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "MainForm";
+			this.Text = "Device Checker";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ DeviceChecker/MainForm.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Read Comments
+ * 
+ * Components
+ *    1. 3 Labels
+ *    2. 1 GroupBox
+ *    3. 1 Button
+ * 
+ * What we are trying to achieve?
+ * -- By clicking on the button it will check the device if it is connected or not. If yes then it will change the color of pictureBox
+ *    to green and further using process we will check its status else it will change color of picturebox to Red.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DeviceChecker
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		/// <summary>
+		/// This is a function through which we pass the commands to cmd.exe and we get a standard output of it.
+		/// Usage:- string value = run_process(@"adb.exe shell getprop ro.secure");
+		/// </summary>
+		/// <param name="Commands"></param>
+		public string run_process(string Commands)
+		{
+			var p = new Process(); // Declaring new process
+		   
+			// Defining the process, setting its parameters
+			p.StartInfo.FileName = "cmd.exe"; // Passing main function to cmd.exe
+			p.StartInfo.Arguments = "/c " + Commands; // Commands string passed here. /c argument is used to passed parameters explicitly.
+			p.StartInfo.CreateNoWindow = true; // No displaying a window
+			p.StartInfo.UseShellExecute = false; // No using cmd.exe to execute shell
+			p.StartInfo.RedirectStandardOutput = true;
+			p.Start(); // Starting the process
+			do 
+			{
+				Application.DoEvents(); // This will prevent application from hang
+			}while(!p.HasExited); // Will wait until process been released from memory
+			return p.StandardOutput.ReadToEnd(); // This will return the output from the process.
+		}
+		
+		public bool isConnected()
+		{
+			var check = run_process("adb.exe devices"); // Will check for connected devices and set it to a string
+			if (String.IsNullOrEmpty(check)) // If string is empty that means no device connected else Device is connected
+			{
+				// If Not Connected
+				pictureBox1.BackColor = Color.Red; // Change Color of Picture Box
+				label1.Text = "Device Not Connected!";
+				status.Text = "---";
+				return false; // Returning boolean value
+			}
+			else
+			{
+				// If Connected, we will also check its status
+				pictureBox1.BackColor = Color.Green;
+				label1.Text = "Device Connected!";
+				status.Text = run_process("adb.exe get-state"); // This will check status of device i.e Android, Recovery, Sideload, etc
+				return true;  // Returning boolean value
+			}
+			
+		}
+		
+		/// <summary>
+		/// By clicking this button we will check our device if it is connected or not.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void Button1Click(object sender, EventArgs e)
+		{
+			if (isConnected())
+			{
+				// It is Connected, do your stuff in this loop
+			}
+		}
+	}
+}
